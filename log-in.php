c6988cca7821b5e9dc45beec5ae092987427f097
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2020-10-18 10:35:47 +0600

    log in page done

---
+++ log-in.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/log-in.css">
+</head>
+<body>
+
+
+    <div class="container">
+        <div class="card w-25 shadow mx-auto">
+            <div class="card-body">
+                <div class="card-title mx-auto">
+                    <h3>Login</h3>
+                </div>
+                <form action="">
+                    <div class="form-group">
+                        <input class="form-control username" type="text" placeholder="Username">
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control password" type="password" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                        <input class="btn btn-info btn-block button"  type="submit" value="Log in">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+<script src="assets/js/jquery-3.5.1.slim.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
