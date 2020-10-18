a1602e4197a79a246e956d61fd06b47ef9f4098c
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2020-10-18 10:47:21 +0600

    Create registration page

---
+++ assets/css/registration.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/">
+</head>
+<body>
+    
+
+
+
+<script src="assets/js/jquery-3.5.1.slim.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ registration.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/registration.css">
+</head>
+<body>
+    <div class="container">
+        <div class="card ">
+            <div class="card-body">
+                <div class="card-title text-center">
+                    <h3>Sign Up</h3>
+                </div>
+                <div class="form-group">
+                    <input class="form-control" type="text" placeholder="Name">
+                </div>
+                <div class="form-group">
+                    <input class="form-control" type="text" placeholder="Email">
+                </div>
+                <div class="form-group">
+                    <input class="form-control" type="text" placeholder="Username">
+                </div>
+                <div class="form-group">
+                    <input class="form-control" type="file" placeholder="Photo">
+                </div>
+                <div class="form-group">
+                    <input class="form-control" type="password" placeholder="Password">
+                </div>
+                <div class="form-group">
+                    <input class="form-control" type="password" placeholder="Confirm Password">
+                </div>
+                <div class="form-group">
+                    <input class="btn btn-success btn-block" value="Sign Up">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+<script src="assets/js/jquery-3.5.1.slim.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
